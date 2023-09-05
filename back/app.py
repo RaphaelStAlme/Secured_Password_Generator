7f8b9e12cd682c4d3af53d2078b66dbaa0dd79cc
luciaLebrun <lebrunlucia@gmail.com>
2023-09-05 15:09:51 +0200

    add: update back

--- back/app.py
+++ back/app.py
@@ -1,15 +1,37 @@
 
 import random
 import string
-from flask import Flask
+import hashlib
+from flask import Flask, request
 
+# Program in Python that generates secure passwords while offering a
+# customization to the user. The program will allow the user to specify
+# the desired length of the password in characters and to choose whether to include numbers or
+# symbols. In addition, optionally, you can integrate a
+# hashing feature using an encryption algorithm of your choice.
 app = Flask(__name__)
 
-@app.route('/generate_password')
-
+@app.route('/generate_password', methods=['GET'])
 def generate_password():
-    password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    args = request.args
+    length = int(args.get('length', 8))
+    digits = args.get('digits', 'true').lower() == 'true'
+    symbols = args.get('symbols', 'true').lower() == 'true'
+    hash = args.get('hash', 'false').lower() == 'true'
+
+    chars = string.ascii_uppercase
+    if digits:
+        chars += string.digits
+    if symbols:
+        chars += string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+
+    if hash:
+        password = hash_password(password)
     return password
+
+def hash_password(password):
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
