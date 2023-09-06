4a436f78021c7ecddcc9ddcc0d815f1e2441d2e1
RaphaelStAlme <raphael_35_974@outlook.fr>
2023-09-06 22:13:05 +0200

    feat: password displayed dynamically

--- front/src/services/password-service.ts
+++ front/src/services/password-service.ts
@@ -2,7 +2,7 @@ export default abstract class PasswordService {
     static async generatePassword(digits: boolean, symbols: boolean, length: string | null, hash: boolean) {
         console.log(length);
         var response = await fetch(`http://127.0.0.1:5000/generate_password?digits=${digits}&symbols=${symbols}&length=${length}&hash=${hash}`);
-        return JSON.parse(await response.text());
+        return response.json();
     }
 }
 
