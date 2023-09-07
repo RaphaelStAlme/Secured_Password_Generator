9eda9484ec5459f087d19cf3c8e2a4163698657b
Raphaël Saint-Alme <51606138+RaphaelStAlme@users.noreply.github.com>
2023-09-07 14:32:58 +0200

    bonus-front (#2)
    * feat: added password checker && added auto copy text to clipboard
    
    * feat: added strength password

--- front/src/services/password-service.ts
+++ front/src/services/password-service.ts
@@ -1,8 +1,7 @@
 export default abstract class PasswordService {
-    static async generatePassword(digits: boolean, symbols: boolean, length: string | null, hash: boolean) {
-        console.log(length);
-        var response = await fetch(`http://127.0.0.1:5000/generate_password?digits=${digits}&symbols=${symbols}&length=${length}&hash=${hash}`);
+    // Method which call API in order to generate a password
+    static async generatePassword(digits: boolean, symbols: boolean, passwordLength: string, hash: boolean) {
+        var response = await fetch(`http://127.0.0.1:5000/generate_password?digits=${digits}&symbols=${symbols}&length=${passwordLength}&hash=${hash}`);
         return response.json();
-    }
+    }    
 }
-
